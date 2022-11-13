96b05aa9245e0bf56365484476ef7fdf83c0bdab
OlgaReut <oshovga@yandex.by>
2022-11-13 21:36:39 +0300

    calc

---
+++ home/calc/calc.js
@@ -0,0 +1,44 @@
+let x = null;
+let y = null;
+
+function getNumber(num) {
+    if(x === null) {
+        x = num
+    } else {
+        y = num
+    }
+}
+
+let sign = '';
+
+function getSign(sign1) {
+    sign = sign1
+}
+
+let res = null;
+function getResult(res) {
+    // let res;
+    switch (sign) {
+        case '+': 
+            res = x+y; 
+            break;
+        case '-':
+            res = x-y;
+            break;
+        case '/':
+            res = x/y;
+            break;
+        case '*':
+            res = x*y;
+            break;
+        default:
+            break;
+    }
+            
+    console.log("result: ", res);
+}
+
+// getResult(x,y,sign);
+// getResult(x,y, '-');
+// getResult(x,y, '/');
+// getResult(x,y, '*');
